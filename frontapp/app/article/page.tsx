4a3af80d4d3407475a03fd32fa94007b0f591dbd
jiseon723 <dighxxx12@gmail.com>
2025-10-16 16:17:15 +0900

    등록 controller 테스트

--- frontapp/app/article/page.tsx
+++ frontapp/app/article/page.tsx
@@ -1,7 +1,8 @@
-export default function Article() {
+export default async function Article() {
+  await fetch("http:/localhost:8090/api/vi/articles");
   return (
     <>
-        <div>게시판</div>
+      <div>게시판</div>
     </>
   );
 }
