f316d651d73b6a5709301c7c41ca9f4c97700559
jiseon723 <dighxxx12@gmail.com>
2025-10-16 16:30:42 +0900

    등록 완료

--- src/main/java/com/rest/proj/domain/article/service/ArticleService.java
+++ src/main/java/com/rest/proj/domain/article/service/ArticleService.java
@@ -2,6 +2,8 @@ package com.rest.proj.domain.article.service;
 
 import com.rest.proj.domain.article.entity.Article;
 import com.rest.proj.domain.article.repository.ArticleRepository;
+import com.rest.proj.global.rsData.RsData;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,20 @@ import java.util.Optional;
 public class ArticleService {
     private final ArticleRepository articleRepository;
 
-    public void create(String subject, String content) {
+    @Transactional //작업 묶음(에러 시 전부취소가 가능, 없을 시 일부저장 방지)
+    public RsData<Article> create(String subject, String content) {
         Article article = Article.builder()
                 .subject(subject)
                 .content(content)
                 .build();
 
         articleRepository.save(article);
+
+        return RsData.of(
+                "S-2",
+                "게시물이 생성되었습니다.",
+                article
+        );
     }
 
     public List<Article> getList() {
